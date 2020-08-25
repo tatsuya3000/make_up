3756cdb3685e420db2aaf4cf3399005ef15baaa4
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-08-25 21:30:13 +0000

    indexとshowの編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+    
+    def index
+        @products = Product.all
+    end
+    
+    def show
+        @products = Product.find(params[:id])
+    end
+    
+    private
+    def produdt_params
+        params.permit(:name, :image_url, :feature, :method)
+    end
+    
 end
