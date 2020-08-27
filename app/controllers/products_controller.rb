6353d32f0746f5d2414db8baa81f50f3e4d7797d
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-08-27 20:59:31 +0000

    コントローラーにsearchアクション追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,11 @@ class ProductsController < ApplicationController
         @products = Product.find(params[:id])
     end
     
+    def search
+        @products1 = Product.where("type1", "%#{params[:type1]}%")
+        @products2 = Product.where("taste1", "%#{params[:taste1]}%")
+    end
+    
     private
     def produdt_params
         params.permit(:name, :image_url, :feature, :method)
