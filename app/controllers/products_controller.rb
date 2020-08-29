5221f6da23a7f9c2c39c94043288885a276e0bd1
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-08-29 00:51:18 +0000

    procucts_controller編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
     
     def search
         @foundations = Product.where("type1", "%#{params[:type1]}%")
-        @lips = Product.where("taste1", "#{params[:taste1]}")
+        @lips = Product.where(taste1: params[:taste1])
     end
     
     private
