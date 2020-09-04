7aecd0ca01501603429f41c6d1ffa63ee5670db8
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-04 19:57:39 +0000

    abilityクラスにread権限追加

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    can :read,  :all
+    
     if user.try(:admin?)
       can :access, :rails_admin
       can :manage, :all
