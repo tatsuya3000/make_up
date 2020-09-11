653268582a956c386eb47a03f685bc04511b8ec4
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-11 20:38:46 +0000

    deviceのストロングパラメータ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+    
     # 各アクションで権限をチェック。オプションでモデル依存をfalseに。
     authorize_resource :class => false
     
