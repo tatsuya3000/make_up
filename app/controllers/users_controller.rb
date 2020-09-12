03af6525b873c168b079bf1354e054d3d56f9e88
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-12 08:14:56 +0000

    userコントローラとshowファイル作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    
+    def show
+        user = User.find(params[:id])
+        @name = user.name
+        @blogs = user.blogs.order("created_at DESC").page(params[:page]).per(5)
+    end
+    
+end
