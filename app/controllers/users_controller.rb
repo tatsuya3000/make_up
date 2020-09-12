d203d2017d8e4e16c47898ca0ce94039bfb9a5ed
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-12 20:20:30 +0000

    ページネーション設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         @name = user.name
-        @products = user.products
+        @products = user.products.order("created_at DESC").page(params[:page]).per(5)
     end
     
 end
