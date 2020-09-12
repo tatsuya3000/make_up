d9a98e2ffcaf729b6f1daaf024e818fa9b48ac0a
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-12 19:42:07 +0000

    user/show編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         @name = user.name
-        @blogs = user.blogs.order("created_at DESC").page(params[:page]).per(5)
+        @products = user.products
     end
     
 end
