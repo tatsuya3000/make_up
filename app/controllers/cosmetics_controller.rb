1c187e15ae3d13d99b8e68ac0550784aaa7ac50d
EC2 Default User <ec2-user@ip-172-31-28-217.ap-northeast-1.compute.internal>
2020-09-12 21:01:58 +0000

    cosmetics_controllerアクション追加

--- app/controllers/cosmetics_controller.rb
+++ app/controllers/cosmetics_controller.rb
@@ -1,2 +1,32 @@
 class CosmeticsController < ApplicationController
+    
+    before_action :move_to_index
+    
+    def index
+        @cosmetics = Cosmetic.all.order("created_at DESC").page(params[:page]).per(5)
+    end
+    
+    def show
+        @cosmetics = Cosmetic.find(params[:id])
+    end
+    
+    def new
+        @cosmetic = Cosmetic.new
+    end
+    
+    def create
+       
+        Cosmetic.create(cosmetic_params)
+        # トップページにリダイレクトする
+        # redirect_to controller: :tops,  action: :index
+    end
+    
+    private
+    def cosmetic_params
+        params.require(:cosmetic).permit(:name, :image, :category, :review).merge(user_id: current_user.id)
+    end
+    
+    def move_to_index
+        redirect_to root_path unless user_signed_in?
+    end
 end
